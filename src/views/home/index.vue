643276b41e4b2b38a1bdec21e6bd9370989d17c5
maya1900 <569760498@qq.com>
2022-04-30 15:50:15 +0800

    fix(index.vue): add button

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="home">
     <h1>{{ msg }} Page</h1>
+    <button @click="get">GET请求</button>
+    <br />
+    <br />
+    <button @click="post">POST请求</button>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    components: {
+export default {
+  name: 'Home',
+  components: {},
+  mixins: [],
+  props: {
+    msg: {
+      type: String,
+      default: 'Home',
     },
-    mixins: [],
-    props: {
-      msg: {
-        type: String,
-        default: 'Home'
-      }
+  },
+  data: function () {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    get() {
+      GET_DATA('/main/list').then((res) => {
+        alert('返回结果：' + res.data);
+      });
     },
-    data: function () {
-      return {
-      }
+    post() {
+      POST_DATA('/main/create', { id: 3433 }).then((res) => {
+        alert('返回结果：' + res.data);
+      });
     },
-    computed: {
-    },
-    watch: {
-    },
-    created() {
-    },
-    mounted() {
-    },
-    methods: {
-    }
-  }
+  },
+};
 </script>
 
-<style lang="scss"scoped>
-  .home {}
+<style lang="scss" scoped>
+.home {
+}
 </style>
